46ea2cae7631072738520410f8c9d7d6d0ad4386
Suryanshu Singh <singhsuryanshu@gmail.com>
2021-09-22 23:55:44 +0530

    TwoSumInputArrayIsSorted

---
+++ DataStructuresAlgorithms/BinarySearch/TwoSumInputArrayIsSorted.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.BinarySearch
+{
+    class TwoSumInputArrayIsSorted
+    {
+        //https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+        //Rename this method to Main to run
+        public static void Main(string[] args)
+        {
+            var numbers = new int[4] { 2, 7, 11, 15 };
+            var a = TwoSum(numbers, 9);
+            Console.WriteLine($"{a[0]}, {a[1]}");
+        }
+
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int start = 0;
+            int end = numbers.Length - 1;
+            while (numbers[start] + numbers[end] != target)
+            {
+                if (numbers[start] + numbers[end] > target)
+                {
+                    end--;
+                }
+                else start++;
+            }
+            return new int[] { start + 1, end + 1 };
+        }
+    }
+}
