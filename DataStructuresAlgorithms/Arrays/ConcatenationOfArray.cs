8ed864ffe8e1af7327d4a41055810b2840ccc686
Suryanshu Singh <surysing@itron.com>
2021-09-27 18:04:40 +0530

    ConcatenationOfArray

---
+++ DataStructuresAlgorithms/Arrays/ConcatenationOfArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Arrays
+{
+    //https://leetcode.com/problems/concatenation-of-array/
+    class ConcatenationOfArray
+    {
+        public static void NotMain(string[] args)
+        {
+            Console.WriteLine();
+        }
+        public static int[] GetConcatenation(int[] nums)
+        {
+            int[] arr = new int[2 * nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr[i] = nums[i];
+                arr[i + nums.Length] = nums[i];
+            }
+            return arr;
+        }
+
+    }
+}
