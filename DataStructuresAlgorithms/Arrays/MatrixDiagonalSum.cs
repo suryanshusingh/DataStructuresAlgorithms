99292d518ded0ca2e17d5d80d2383026726a3446
Suryanshu Singh <surysing@itron.com>
2021-09-28 15:00:42 +0530

    Matrix Diagonal Sum

---
+++ DataStructuresAlgorithms/Arrays/MatrixDiagonalSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Arrays
+{
+    // https://leetcode.com/problems/matrix-diagonal-sum/
+    //Rename this method to Main to run
+    class MatrixDiagonalSum
+    {
+        private static void NotMain(string[] args)
+        {
+
+        }
+
+        public static int DiagonalSum(int[][] mat)
+        {
+            int sum = 0;
+            int n = mat.Length;
+            for (int i = 0; i < n; i++)
+            {
+                sum += mat[i][i];
+                sum += mat[i][n - i - 1];
+            }
+            if (n % 2 == 1) sum -= mat[n / 2][n / 2];
+            return sum;
+        }
+    }
+}
