09c770b3eef708c75ca8ebd2d4cbd860c568cb86
Suryanshu Singh <surysing@itron.com>
2021-09-29 14:32:19 +0530

    Two Sum

---
+++ DataStructuresAlgorithms/Arrays/TwoSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Arrays
+{
+    class TwoSum
+    {
+        //https://leetcode.com/problems/two-sum/
+        public static void NotMain(string[] args)
+        {
+
+        }
+
+
+        public int[] TwoSumFunc(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { dict[target - nums[i]], i };
+                }
+
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+    }
+}
