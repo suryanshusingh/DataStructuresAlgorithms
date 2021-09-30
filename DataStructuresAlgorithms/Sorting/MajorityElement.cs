0d89f074755adf00c1536f538a4c9a7a0f69a74a
Suryanshu Singh <surysing@itron.com>
2021-10-01 01:00:49 +0530

    Majority Element

---
+++ DataStructuresAlgorithms/Sorting/MajorityElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Sorting
+{
+    class MajorityElement
+    {
+        // Important
+        //https://leetcode.com/problems/majority-element/
+        public static void NotMain(string[] args)
+        {
+
+        }
+
+        public int FindMajorityElement(int[] nums)
+        {
+            int count = 0;
+            int answer = 0;
+            foreach (var num in nums)
+            {
+                if (count == 0)
+                {
+                    answer = num;
+                }
+                if (answer == num) count++;
+                else count--;
+            }
+            return answer;
+        }
+    }
+}
