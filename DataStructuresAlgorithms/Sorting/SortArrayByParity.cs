7a65ab8b12e8548efb59081e96f5888377887be2
Suryanshu Singh <surysing@itron.com>
2021-10-02 14:33:19 +0530

    Sort Array By parity

---
+++ DataStructuresAlgorithms/Sorting/SortArrayByParity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Sorting
+{
+    class SortArrayByParity
+    {
+        //https://leetcode.com/problems/sort-array-by-parity/
+        public static void NotMain(string[] args)
+        {
+
+        }
+
+        public int[] SortArrayByParity1(int[] nums)
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+            while (start < end)
+            {
+                if (nums[start] % 2 == 0)
+                {
+                    start++;
+                }
+                else
+                {
+                    if (nums[end] % 2 == 0)
+                    {
+                        swap(nums, start, end);
+                        start++; end--;
+                    }
+                    else
+                    {
+                        end--;
+                    }
+                }
+            }
+            return nums;
+
+        }
+
+        public void swap(int[] arr, int first, int second)
+        {
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+    }
+}
