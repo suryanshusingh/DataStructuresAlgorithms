11fe404bd3b1cd492b4176ad647754642612be82
Suryanshu Singh <surysing@itron.com>
2021-10-02 14:31:49 +0530

    Missing Number

---
+++ DataStructuresAlgorithms/Sorting/MissingNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Sorting
+{
+    class MissingNumber
+    {
+        //https://leetcode.com/problems/missing-number/
+        public static void NotMain(string[] args)
+        {
+
+        }
+
+        // 1. Cyclic Sort and then search the missing number  - O(n) complexity O(1) space
+        // 2. Use gaussian sum formula - n*(n+1)/2 - sum = result
+        // 3. XOR solution
+        public int MissingNumber1(int[] nums)
+        {
+            int i = 0;
+            while (i < nums.Length)
+            {
+                int correctIndex = nums[i];
+                if (nums[i] < nums.Length && nums[i] != nums[correctIndex])
+                {
+                    Swap(nums, i, correctIndex);
+                }
+                else i++;
+            }
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (j != nums[j]) return j;
+            }
+            return nums.Length;
+        }
+
+        public void Swap(int[] arr, int first, int second)
+        {
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+    }
+}
