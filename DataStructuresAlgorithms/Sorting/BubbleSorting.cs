13d9602d3e2d181dfd5ecdc9b278051d9c525f0e
Suryanshu Singh <singhsuryanshu@gmail.com>
2021-10-03 00:09:31 +0530

    Bubble sort

---
+++ DataStructuresAlgorithms/Sorting/BubbleSorting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Sorting
+{
+    class BubbleSorting
+    {
+
+        public static void NotMain(string[] args)
+        {
+            //int[] arr = [1, 2, 4, 3, 7, 6, 0, 8, 5];
+
+
+        }
+        public static void BubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        swapped = true;
+                    }
+                }
+                if (swapped == false)
+                {
+                    break;
+                }
+            }
+        }
+
+    }
+
+    
+}
