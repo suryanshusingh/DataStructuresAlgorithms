1a2c7bad02eb94153028493e00fd28735185b15b
Suryanshu Singh <singhsuryanshu@gmail.com>
2021-10-20 22:51:22 +0530

    SumTriangle

---
+++ DataStructuresAlgorithms/Oops/Static.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms
+{
+    class Static
+    {
+        // The main method is always static because we dont want the c# complier to create an object first.
+        public static void NotMain(string[] args)
+        {
+            A obj = new A();
+            obj.IncreaseBy1();
+            Console.WriteLine(obj.ValueOfa()); //11
+
+            A obj2 = new A();
+            obj2.IncreaseBy1();
+            Console.WriteLine(obj.ValueOfa()); //12
+
+            A.StaticMethod();
+
+            Console.WriteLine("print");
+        }
+
+
+    }
+
+    class A
+    {
+        // This cannot be accessed in c#, but can be accessed in java
+        // This would be allocated memory only once for all the objects of this class, so if value is increased once, it will be increased at another places also.
+        public static int a = 10;
+
+        public int aa = 10;
+
+        public void IncreaseBy1()
+        {
+            a++;
+        }
+
+        public int ValueOfa()
+        {
+            return a;
+        }
+
+        // this can be called without making any object.
+        // Static methods cannot access non static variables.
+        public static void StaticMethod()
+        {       
+            var b = a;
+            
+            //Error
+            //var bb = aa;
+        }
+    }
+}
---
+++ DataStructuresAlgorithms/Recursion/SumTriangleFromArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.Recursion
+{
+    class SumTriangleFromArray
+    {
+        public static void NotMain(string[] args)
+        {
+            int[] A = { 1, 2, 3, 4, 5 };
+            SumTriangle(A);
+
+        }
+
+        private static void SumTriangle(int[] arr)
+        {
+            if(arr.Length < 2)
+            {
+                Console.WriteLine(arr[0]);
+                return;
+            }
+
+            //print array
+            
+            int[] arr1 = new int[arr.Length - 1];
+            for (int i = 0; i <arr.Length - 1; i++)
+            {
+                arr1[i] = arr[i] + arr[i + 1];
+            }
+            SumTriangle(arr1);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
