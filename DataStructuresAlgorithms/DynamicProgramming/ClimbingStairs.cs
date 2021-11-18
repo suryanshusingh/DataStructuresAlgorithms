d8cffb3df8cf400185682286b890605910198191
Suryanshu Singh <singhsuryanshu@gmail.com>
2021-11-19 02:54:31 +0530

    DP Climbing stairs

---
+++ DataStructuresAlgorithms/DynamicProgramming/ClimbingStairs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAlgorithms.DynamicProgramming
+{
+    class ClimbingStairs
+    {
+        //Leetcode: leetcode.com/problems/climbing-stairs
+        public static void NotMain(string[] args)
+        {
+            Console.WriteLine(ClimbStairs(5));
+        }
+
+        private static int ClimbStairs(int n)
+        {
+            if (n == 0 || n == 1 || n == 2) return n;
+            int stepOne = 1;
+            int stepTwo = 2;
+            for (int i = 2; i < n; i++)
+            {
+                int result = stepOne + stepTwo;
+                stepOne = stepTwo;
+                stepTwo = result;
+            }
+            return stepTwo;
+        }
+    }
+}
